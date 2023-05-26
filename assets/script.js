0e2683fac11842f8b92b9a0ca3f43b37f8f5c004
ChristopherSMM <129230277+ChristopherSMM@users.noreply.github.com>
2023-05-25 21:08:57 -0700

    replaced fetch value and properly linked java

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
+// Api Request
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
-var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
+var temperature = document.querySelector('.temp');
+var description = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
+
 button.addEventListener('click', function(name){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
 .then(response => response.json())
